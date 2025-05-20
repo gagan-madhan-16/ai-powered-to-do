268ac57d8418a34a80a93e3f186505c44a1a54bd
Gagan Madhan <158490863+gagan-madhan-16@users.noreply.github.com>
2025-05-20 23:02:48 +0530

    Update next.config.ts

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['localhost'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // API proxying
   async rewrites() {
     return [
